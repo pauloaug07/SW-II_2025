0f183ab39fc90a9b3f34b057215a7eb1d7b74da5
pauloaug07 <paulo.costa181@etec.sp.gov.br>
2025-04-29 12:35:08 -0300

    rota PUT e DELETE na API

--- aula06e08-API/api.php
+++ aula06e08-API/api.php
@@ -38,7 +38,6 @@ switch ($metodo) {
             // retorna todos os usuarios
             echo json_encode($usuarios, JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE);
         }
-
         break;
 
     case 'POST':
@@ -76,6 +75,85 @@ switch ($metodo) {
             ],
             JSON_UNESCAPED_UNICODE
         );
+        break;
+
+    case 'PUT':
+        // Verifica se há o ID na URL
+        if (isset($_GET["id"])) {
+            $id = intval($_GET["id"]);
+            $dados = json_decode(file_get_contents('php://input'), true);
+
+            // Verifica se o usuário existe
+            $usuario_encontrado = null;
+            foreach ($usuarios as &$usuario) {
+                if ($usuario['id'] == $id) {
+                    $usuario_encontrado = &$usuario;
+                    break;
+                }
+            }
+
+            if ($usuario_encontrado) {
+                // Atualiza os dados do usuário
+                if (isset($dados["nome"]))
+                    $usuario_encontrado["nome"] = $dados["nome"];
+                if (isset($dados["email"]))
+                    $usuario_encontrado["email"] = $dados["email"];
+
+                // Salva o arquivo com a atualização
+                file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE));
+
+                echo json_encode(
+                    [
+                        "mensagem" => "Usuário atualizado com sucesso!",
+                        "usuario" => $usuario_encontrado
+                    ],
+                    JSON_UNESCAPED_UNICODE
+                );
+            } else {
+                http_response_code(404);
+                echo json_encode(["erro" => "Usuário não encontrado para atualização"], JSON_UNESCAPED_UNICODE);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(["erro" => "ID não fornecido para atualização"], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
+    case 'DELETE':
+        // Verifica se há o ID na URL
+        if (isset($_GET["id"])) {
+            $id = intval($_GET["id"]);
+            $usuario_encontrado = null;
+            $usuarios_novos = [];
+
+            // Verifica se o usuário existe e remove
+            foreach ($usuarios as $usuario) {
+                if ($usuario['id'] != $id) {
+                    $usuarios_novos[] = $usuario;
+                } else {
+                    $usuario_encontrado = $usuario;
+                }
+            }
+
+            if ($usuario_encontrado) {
+                // Atualiza o arquivo sem o usuário deletado
+                file_put_contents($arquivo, json_encode($usuarios_novos, JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE));
+
+                echo json_encode(
+                    [
+                        "mensagem" => "Usuário removido com sucesso!",
+                        "usuario_removido" => $usuario_encontrado
+                    ],
+                    JSON_UNESCAPED_UNICODE
+                );
+            } else {
+                http_response_code(404);
+                echo json_encode(["erro" => "Usuário não encontrado para remoção"], JSON_UNESCAPED_UNICODE);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(["erro" => "ID não fornecido para remoção"], JSON_UNESCAPED_UNICODE);
+        }
         break;
 
     default:
